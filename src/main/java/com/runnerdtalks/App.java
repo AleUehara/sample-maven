054af46aa47482b3c53f7ced8b43180f54b46a35
Alexandre Uehara <alexandreuehara@Alexandres-MacBook-Pro.local>
2011-11-20 13:08:43 -0200

    first commmit - teste maven

---
+++ src/main/java/com/runnerdtalks/App.java
@@ -0,0 +1,13 @@
+package com.runnerdtalks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
